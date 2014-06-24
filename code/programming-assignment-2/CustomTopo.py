de7296672b8e15678d018ce454534194814363d5
hemantku <vgh4uster@gmail.com>
2014-06-24 15:08:29 -0700

    Week 3

--- code/programming-assignment-2/CustomTopo.py
+++ code/programming-assignment-2/CustomTopo.py
@@ -19,7 +19,24 @@ class CustomTopo(Topo):
         Topo.__init__(self, **opts)
         
         # Add your logic here ...
+	# Numbering: h1..N, s1..M
+        self.hostNum = 1
+        self.switchNum = 1
+        self.addTree([linkopts1, linkopts2, linkopts3], fanout);
+ 
+    # Derived from class TreeTopo
+    def addTree(self, linkopts, fanout, depth=0):
+        isSwitch = depth < 3
+        if isSwitch:
+            node = self.addSwitch('s%s' % self.switchNum)
+            self.switchNum += 1
+            for _ in range(fanout):
+                child = self.addTree(linkopts, fanout, depth + 1)
+                self.addLink(node, child, **linkopts[depth])
+        else:
+            # host
+            node = self.addHost('h%s' % self.hostNum)
+            self.hostNum += 1
+        return node 
 
-        
-                    
 topos = { 'custom': ( lambda: CustomTopo() ) }
